6ea6873049469081b2de3cf4397715c23a8028fa
Marcus <marcusmonteiro99@gmail.com>
2025-02-20 23:35:27 +0300

    Added light and dark modes

---
+++ js/dark-light-elements.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const logo = document.querySelector(".unitrack-logo");
+
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        logo.src = "../assets/imgs/unitrack-images/unitrack-logo-text-white.png";
+    } else {
+        logo.src = "../assets/imgs/unitrack-images/unitrack-logo-text-black.png";
+    }
+
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
+        logo.src = e.matches
+            ? "../assets/imgs/unitrack-images/unitrack-logo-text-white.png"
+            : "../assets/imgs/unitrack-images/unitrack-logo-text-black.png";
+    });
+});
