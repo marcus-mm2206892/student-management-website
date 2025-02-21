907509dbf0ebc0774a5be386fd70fd2ab5ff00aa
Marcus <marcusmonteiro99@gmail.com>
2025-02-21 20:36:09 +0300

    Finished footer

--- scripts/dark-light-elements.js
+++ scripts/dark-light-elements.js
@@ -1,15 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const logo = document.querySelector(".unitrack-logo-text");
+    const logotext = document.querySelector(".unitrack-logo-text");
+    const logo = document.querySelector(".unitrack-logo");
 
+    // logotext
     if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        logo.src = "../assets/imgs/unitrack-images/unitrack-logo-text-white.png";
+        logotext.src = "../assets/imgs/unitrack-images/unitrack-logo-text-white.png";
     } else {
-        logo.src = "../assets/imgs/unitrack-images/unitrack-logo-text-black.png";
+        logotext.src = "../assets/imgs/unitrack-images/unitrack-logo-text-black.png";
+    }
+
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
+        logotext.src = e.matches
+            ? "../assets/imgs/unitrack-images/unitrack-logo-text-white.png"
+            : "../assets/imgs/unitrack-images/unitrack-logo-text-black.png";
+    });
+
+    // logo
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        logo.src = "../assets/imgs/unitrack-images/unitrack-logo-white.png";
+    } else {
+        logo.src = "../assets/imgs/unitrack-images/unitrack-logo-blue.png";
     }
 
     window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
         logo.src = e.matches
-            ? "../assets/imgs/unitrack-images/unitrack-logo-text-white.png"
-            : "../assets/imgs/unitrack-images/unitrack-logo-text-black.png";
+            ? "../assets/imgs/unitrack-images/unitrack-logo-white.png"
+            : "../assets/imgs/unitrack-images/unitrack-logo-blue.png";
     });
 });
