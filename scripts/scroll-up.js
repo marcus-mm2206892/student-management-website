e68cd8056048c3b0320a746d38cedd6e178792b0
Marcus <marcusmonteiro99@gmail.com>
2025-02-23 06:08:23 +0300

    Added learning path and added scroll up button on page

---
+++ scripts/scroll-up.js
@@ -0,0 +1,16 @@
+const scrollUpBtn = document.getElementById("scrollUpBtn");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        scrollUpBtn.classList.add("show");
+    } else {
+        scrollUpBtn.classList.remove("show");
+    }
+});
+
+scrollUpBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
