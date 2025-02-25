d6ccec32aaa07cbe1dac8cafa2491f0baf426544
bashar <muhtasim2k2@gmail.com>
2025-02-25 22:48:00 +0300

    Reading data from courses.json

---
+++ scripts/load-courses-to-register.js
@@ -0,0 +1,27 @@
+fetch("../assets/data/courses.json")
+.then( function(response) {
+    return response.json();
+})
+.then( function(allCourses) {
+    let placeholder = document.querySelector('#data-output');
+    let out = "";
+    for(let course of allCourses){
+        out += `
+        <tr>
+            <td class="course-no"> ${course.courseId} </td>
+            <td> ${course.courseName} </td>
+            <td> ${course.courseName}  </td>
+            <td> L01 </td>
+            <td> 33/40 </td>
+            <td> <p class="status"> Waiting for approval </p>  </td>
+            <td> MON-WED </td>
+            <td>
+                <button> <strong> Register </strong> </button> 
+            </td>
+        </tr>
+
+        `
+    }
+
+    placeholder.innerHTML = out;
+})
