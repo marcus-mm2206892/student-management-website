f5870dd28c7d38e40cac6bfa8bb206d8fd8ec15d
josh-jc2203604 <joshcalma21@yahoo.com>
2025-02-27 23:05:26 +0300

    updates to grade-submission webpage

---
+++ scripts/grades-submission.js
@@ -0,0 +1,4 @@
+function toggleDropdown() {
+    const dropdownMenu = document.querySelector(".dropdown-menu");
+    dropdownMenu.style.display = dropdownMenu.style.display === "block" ? "none" : "block";
+}
