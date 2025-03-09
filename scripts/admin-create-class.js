a0327ced8f1d92bdca789728ec38461eaa87f47c
Audre-202212698 <ac2212698@qu.edu.qa>
2025-03-09 23:40:20 +0300

    Update courses.json
    removed campus, majors.
    added course description

--- scripts/admin-create-class.js
+++ scripts/admin-create-class.js
@@ -1,35 +1,29 @@
-// I don't know why, but I can't get the external js to apply to the html. Something about the browser?
-// Either way, I'm copy pasting this code into the html file for now.
-      //figuring out how to save to json ->https://www.geeksforgeeks.org/read-json-file-using-javascript/
-//       fetch('../data/courses.json').then(response => response.json()).then(courses => {
-//         const courseDropdown = document.getElementById('courseId');
-//         courses.forEach(course => {
-//         const option = document.createElement('option');
-//         option.value = course.courseId;
-//         option.textContent = `${course.courseName} (${course.courseId})`;
-//         courseDropdown.appendChild(option);
-//     });
-//    });
-//     document.getElementById('createClassForm').addEventListener('submit', function(event) {
-//     const classId = document.getElementById('classId').value;
-//     const instructor = document.getElementById('instructor').value;
-//     const maxStudents = parseInt(document.getElementById('maxStudents').value);
-//     const classStatus = false; //default value
-//     const schedule = "" //empty for now, probably add calendar later once we clarify what use case 8 is?
-//     const campus = document.querySelector('input[name="campus"]:checked').value;
+//figuring out how to save to json ->https://www.geeksforgeeks.org/read-json-file-using-javascript/
+        // fetch('../data/courses.json').then(response => response.json()) // Parse JSON
+        //     .then(data => console.log(data)) // Work with JSON data
+        //     .catch(error => console.error('Error fetching JSON:', error));
 
-//     const newClass = {
-//         classId: classId,
-//         instructor: instructor,
-//         maxStudents: maxStudents,
-//         classStatus: false,
-//         Students: [""], // default value
-//         Schedule: schedule,
-//         campus: campus
-//     };
-//     alert('Class created successfully!');
-//     document.getElementById('createClassForm').reset(); // Clear the form
-//     });
+        // document.getElementById('createClassForm').addEventListener('submit', function(event) {
+        //     const classId = document.getElementById('classId').value;
+        //     const instructor = document.getElementById('instructor').value;
+        //     const maxStudents = parseInt(document.getElementById('maxStudents').value);
+        //     const classStatus = false; //default value
+        //     const schedule = "" //empty for now, probably add calendar later once we clarify what use case 8 is?
+        //     const campus = document.querySelector('input[name="campus"]:checked').value;
+
+        //     const newClass = {
+        //         classId: classId,
+        //         instructor: instructor,
+        //         maxStudents: maxStudents,
+        //         classStatus: false,
+        //         Students: [""], // default value
+        //         Schedule: schedule,
+        //         campus: campus
+        //     };
+
+            
+        //     document.getElementById('createClassForm').reset(); // Clear the form
+        // });
 
 const jsonData = import ('../data/courses.json');
 console.log(jsonData);
