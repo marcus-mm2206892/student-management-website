405301fbe9e750eb825c17f2e4289d2037b794f4
josh-jc2203604 <joshcalma21@yahoo.com>
2025-04-03 17:10:09 +0300

    update

--- scripts/admin-create-course.js
+++ scripts/admin-create-course.js
@@ -1,34 +1,49 @@
  document.addEventListener("DOMContentLoaded", function() {
     let courseForm=document.querySelector('#create-course-form');
 
-    let allMajors = [];
-    let allCourses = [];
-
-    // Fetching Majors...
-    fetch("../assets/data/majors.json")
-        .then(res => res.json())
-        .then(data => {
-            allMajors = data;
-        });
-
-    // Fetching Courses...
-    fetch("../assets/data/courses.json")
-        .then(res => res.json())
-        .then(data => {
-            allCourses = data;
-        });
-
     // Fetch both JSON files
     Promise.all([
-        fetch("../assets/data/major.json").then(res => res.json()),
-        fetch("../assets/courses.json").then(res => res.json())
+        fetch("../assets/data/majors.json").then(res => res.json()),
+        fetch("../assets/data/courses.json").then(res => res.json())
     ])
     .then(([majors, courses]) => {
         console.log("Majors Loaded:", majors);
         console.log("Courses Loaded:", courses);
-        
 
+        document.querySelector("#subjectDropdown").innerHTML = majors.map(major =>
+            `<div onclick="selectOption('Subject')"><i class="fas fa-book"></i>${major.majorName}</div>`
+        ).join(' ');
+
+        document.querySelector("#prereqDropdown").innerHTML = courses.map(course =>
+            `<div onclick="addTag(this, 'prereq')">${course.courseId}</div>`
+        ).join(' ');
+
+        document.querySelector("#majorsDropdown").innerHTML = majors.map(major =>
+            `<div onclick="addTag(this, 'major')">${major.majorName}</div>`
+        ).join(' ');
     })
+
+
+
+    document.querySelectorAll(".dropdown-toggle").forEach(toggle => {
+        toggle.addEventListener("click", function (event) {
+            event.stopPropagation();
+            let dropdownMenu = this.nextElementSibling;
+            let rect = this.getBoundingClientRect();
+  
+            document.querySelectorAll(".dropdown-menu").forEach(menu => {
+                if (menu !== dropdownMenu) menu.style.display = "none";
+            });
+  
+            let isOpen = dropdownMenu.style.display === "block";
+            dropdownMenu.style.display = isOpen ? "none" : "block";
+  
+            if (!isOpen) {
+                dropdownMenu.style.setProperty("--dropdown-top", `${rect.bottom}px`);
+                dropdownMenu.style.setProperty("--dropdown-left", `${rect.left}px`);
+            }
+        });
+    });
 
     document.addEventListener("submit", function (event) {
         event.preventDefault();
